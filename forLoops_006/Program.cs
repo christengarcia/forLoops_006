7d410fb5c9005f363e5b6af434f695cdcd82287e
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-18 10:31:33 +1100

    First commit

---
+++ forLoops_006/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace forLoops_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 10; ++i)
+            {
+                Console.Write("Happy Birthday! ");
+            }
+            Console.WriteLine("\nTaDa!");
+        }
+    }
+}
